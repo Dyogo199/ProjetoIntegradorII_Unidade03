6a1a6a6da741cef742bf03903229d3bad5735fec
Dyogo Mondego Moraes <41173605+Dyogo199@users.noreply.github.com>
2025-07-04 10:03:31 -0300

    Update livraria.sql

--- livraria.sql
+++ livraria.sql
@@ -12,17 +12,19 @@ CREATE TABLE pedidos (
     FOREIGN KEY (produto_id) REFERENCES produtos(id)
 );
 
--- Inserção de produtos
+-- Inserção de produtos (agora com 4 produtos)
 INSERT INTO produtos (nome, preco) VALUES
 ('Livro de SQL', 59.90),
 ('HTML e CSS na Prática', 44.50),
-('JavaScript Essencial', 69.00);
+('JavaScript Essencial', 69.00),
+('Python para Iniciantes', 75.20); -- produto adicionado
 
 -- Inserção de pedidos
 INSERT INTO pedidos (produto_id, quantidade, data_pedido) VALUES
 (1, 2, '2025-07-10'),
 (2, 1, '2025-07-10'),
-(3, 3, '2025-07-11');
+(3, 3, '2025-07-11'),
+(4, 1, '2025-07-12'); -- pedido para o novo produto
 
 -- Exibir os dados inseridos
 SELECT * FROM produtos;
